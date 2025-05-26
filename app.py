fdef1880307a8ec2a181b97a249f358a013a3378
Katherine Chen <katherinechen@KatherinedeMacBook-Pro.local>
2025-05-25 17:04:09 -0700

    basic features completed

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/habit-tracker')
+def habit_tracker():
+    return render_template("habit_tracker.html")
+
+@app.route('/tomato-clock')
+def tomato_clock():
+    return render_template("tomato_clock.html")
+
+@app.route('/todo-list')
+def todo_list():
+    return render_template("todo_list.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
